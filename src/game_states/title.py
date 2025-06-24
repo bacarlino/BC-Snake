7d305244f78e8fcf412b5e80682c3403967c4fbb
Brandon Carlino <bacarlino@gmail.com>
2025-06-24 06:01:39 -0400

    re-commit unsaved changes in refactor

--- src/game_states/title.py
+++ src/game_states/title.py
@@ -12,14 +12,12 @@ class Title(GameState):
     def __init__(self, game):
         super().__init__(game)
         self.ui = TitleUI()
-
         
         self.commands = {
             MenuInput.SELECT: False
         }
 
     def handle_events(self, event):
-        # invoker??
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                self.commands[MenuInput.SELECT] = True
@@ -27,25 +25,9 @@ class Title(GameState):
     def update(self):
         if self.commands[MenuInput.SELECT]:
             MENU_SELECT.play()
-            self.game.game_state.pop()
-            self.game.game_state.push(TitleMenu(self.game))
+            self.game.game_state.transition_to(TitleMenu(self.game))
 
         self.reset_command_flags()
 
     def draw(self, window):
         self.ui.draw(window)
-        # window.blit(ui.PING_PANG_SURF, ui.PING_PANG_RECT)
-        # window.blit(ui.TITLE_SURF, ui.TITLE_RECT)
-        # window.blit(ui.PRESS_SPACE_SURF, ui.PRESS_SPACE_RECT)
-
-        
-
-class PushTitleMenu:
-    def __init__(self, state):
-        self.state = state
-
-    def execute(self):
-        MENU_SELECT.play()
-        self.state.game.game_state.pop()
-        self.game.game_state.push(TitleMenu(self.game))
-
